d3150044e8839cb50f1ddc8a3399d5875a62afcf
홍준희 <junheehong@naver.com>
2024-11-24 00:47:47 +0900

    제발좀

--- autoprompt-modified/autoprompt/create_trigger.py
+++ autoprompt-modified/autoprompt/create_trigger.py
@@ -515,13 +515,18 @@ def evaluate_candidates(model, predictor, dev_loader, averaged_grad, trigger_ids
     # 검증 데이터셋을 이용해 각 후보 점수 계산
     for candidate in candidates:
         temp_trigger_ids = trigger_ids.clone()  # 트리거 복사
-        temp_trigger_ids[:, token_to_flip] = candidate  # 특정 위치의 토큰 변경
 
 
-        
         logger.debug(f"temp_trigger_ids shape: {temp_trigger_ids.shape}")  # 추가
         logger.debug(f"temp_trigger_ids[:, token_to_flip] shape: {temp_trigger_ids[:, token_to_flip].shape}")  # 추가
         logger.debug(f"candidate shape: {candidate.shape}")  # 추가
+
+        
+        temp_trigger_ids[:, token_to_flip] = candidate  # 특정 위치의 토큰 변경
+
+
+
+        
 
 
 
